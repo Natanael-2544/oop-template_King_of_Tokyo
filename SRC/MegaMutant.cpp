debe59f28c9ae1003a2dc786278495e85228239a
Natanael-2544 <natanael.ghiura@s.unibuc.ro>
2025-12-30 17:57:28 +0200

    Update

--- SRC/MegaMutant.cpp
+++ SRC/MegaMutant.cpp
@@ -7,17 +7,10 @@
 MegaMutant::MegaMutant(const std::string& n)
     : Monstru(n, 9), Mutant(n), Dragon(n), Robot(n) {}
 
-void MegaMutant::atac(Monstru* t) {
-    if (!t) return;
-    *t -= 2;
-    *this += 2;
-}
-
 void MegaMutant::vindecare() {
     viata += 1;
     if (viata > 9) viata = 9;
 }
 
-void MegaMutant::folosestePutere() {
-    *this += 3;
-}
+[[maybe_unused]] void MegaMutant::atac(Monstru* /*t*/) { }
+[[maybe_unused]] void MegaMutant::folosestePutere() { }
